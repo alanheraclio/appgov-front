3dfd921e97450921de66fb4877e545f367a0f235
alanheraclio <alanheraclio@gmail.com>
2018-11-13 11:55:38 -0600

    http delete added

--- js/controllers/delete-table-data.js
+++ js/controllers/delete-table-data.js
@@ -1,5 +1,14 @@
 console.log("delete-table-data Running...");
+var urlAPI = "http://159.65.98.32/api/obras/";
 
 function deleteRecord(recordId){
-  alert("Seguro que deseas borrar el record: "+recordId);
+  alert("Seguro que deseas borrar el record: "+recordId.replace("delete-", ""));
+  function deleteData() {
+	  return fetch(urlAPI+recordId.replace("delete-", ""), {
+	    method: 'delete'
+	  })
+	  .then(response => response.json());
+	}
+
+	deleteData();
 }
